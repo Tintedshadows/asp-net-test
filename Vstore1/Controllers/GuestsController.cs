a2bab6802225640352ff32b371566c2fef45240d
Mike McGraw <mike.mcgraw@hotmail.com>
2018-10-29 13:22:34 -0500

    First Commit

---
+++ Vstore1/Controllers/GuestsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vstore1.Models;
+
+namespace Vstore1.Controllers
+{
+    public class GuestsController : Controller
+    {
+        [Route("Guests/Index")]
+        public ActionResult Index()
+        {
+            var guests = new List<Guest>
+            {
+                new Guest { Fname = "Bob", Lname = "Bobberson", Id = 11},
+                new Guest { Fname = "Jill", Lname = "Jillson", Id = 12}
+            };
+
+            return View(guests);
+        }
+    }
+}
---
+++ Vstore1/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vstore1.Models;
+
+namespace Vstore1.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Guest> Guests { get; set; }
+    }
+}
